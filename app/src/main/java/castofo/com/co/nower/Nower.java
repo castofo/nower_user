ef81af39e1ae417860070bca8a947f5a77d2c4aa
ibalejandro <asanch41@eafit.edu.co>
2017-05-14 22:14:23 -0500

    Adding the Realm dependencies to the project and configuring the Application class to use Stetho to manage the db.

---
+++ app/src/main/java/castofo/com/co/nower/Nower.java
@@ -0,0 +1,36 @@
+package castofo.com.co.nower;
+
+import android.app.Application;
+
+import com.facebook.stetho.Stetho;
+import com.uphyca.stetho_realm.RealmInspectorModulesProvider;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
+/**
+ * Created by alejandrosanchezaristizabal on 5/14/17.
+ */
+
+public class Nower extends Application {
+  @Override
+  public void onCreate() {
+    super.onCreate();
+
+    Realm.init(this);
+
+    /*
+    RealmConfiguration config = new RealmConfiguration.Builder()
+        .schemaVersion(0) // Must be manually bumped when the schema changes.
+        .migration(new MigrationManager())
+        .build();
+    Realm.setDefaultConfiguration(config);
+    */
+
+    // Stetho initialization is needed to see the db on the Chrome inspector.
+    Stetho.initialize(Stetho.newInitializerBuilder(this)
+        .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
+        .enableWebKitInspector(RealmInspectorModulesProvider.builder(this).build())
+        .build());
+  }
+}
--- app/src/main/java/castofo/com/co/nower/models/Branch.java
+++ app/src/main/java/castofo/com/co/nower/models/Branch.java
@@ -1,10 +1,16 @@
 package castofo.com.co.nower.models;
 
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+import io.realm.annotations.Required;
+
 /**
  * Created by Alejandro on 29/10/2016.
  */
-public class Branch {
+public class Branch extends RealmObject {
 
+  @PrimaryKey
+  @Required
   private String id;
   private double latitude;
   private double longitude;
